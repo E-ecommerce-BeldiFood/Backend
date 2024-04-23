96a3ca57e61ad8c0add552e44dac6c74ca2668e4
Noura boudra <noura.boudra1@gmail.com>
2024-04-23 15:29:39 +0100

    noura trying to add her changes

--- gateway-service/src/main/java/ma/beldifood/gateway/security/GatewayConfig.java
+++ gateway-service/src/main/java/ma/beldifood/gateway/security/GatewayConfig.java
@@ -1,10 +1,7 @@
 package ma.beldifood.gateway.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,14 +11,12 @@ public class GatewayConfig {
     @Autowired
     AuthenticationFilter filter;
 
-    @Bean
+    /*@Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("User-management-microservice", r -> r.path("/auth/**")
                         .uri("lb://security-service"))
-
-
-                .build();
-    }
+                        .build();
+    }*/
 
 }
